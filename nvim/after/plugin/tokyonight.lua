19cbf58aed9eefd229fce351f4f4869d01ab9f26
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2024-02-24 10:03:03 +0200

    tokyonight line numbesr

--- nvim/after/plugin/tokyonight.lua
+++ nvim/after/plugin/tokyonight.lua
@@ -1,4 +1,9 @@
 vim.opt.termguicolors = true
 require('tokyonight').setup({
+    on_colors = function(colors)
+        vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = colors.comment })
+        vim.api.nvim_set_hl(0, 'LineNr', { fg = colors.fg_sidebar })
+        vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = colors.comment })
+    end
 })
 vim.cmd.colorscheme('tokyonight')
