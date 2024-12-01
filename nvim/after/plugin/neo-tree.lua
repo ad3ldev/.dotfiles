221437df244dd0f178e907880bb7bc198630c0de
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2024-12-02 00:57:26 +0200

    different files show

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -4,8 +4,13 @@ require("neo-tree").setup({
 			visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
 			hide_dotfiles = false,
 			hide_gitignored = false,
+			hide_by_name = {
+				"node_modules",
+			},
 			never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
 				".DS_Store",
+				".git",
+				"__pycache__",
 			},
 		},
 	},
