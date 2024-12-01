d5cac82dbc7b7486e0de91862496fdb6a2e213d7
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2024-12-02 00:40:37 +0200

    changed neo tree

---
+++ nvim/after/plugin/neo-tree.lua
@@ -0,0 +1,12 @@
+require("neo-tree").setup({
+	filesystem = {
+		filtered_items = {
+			visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+			hide_dotfiles = false,
+			hide_gitignored = false,
+			never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+				".DS_Store",
+			},
+		},
+	},
+})
--- nvim/lua/adele/plugins/neo-tree.lua
+++ nvim/lua/adele/plugins/neo-tree.lua
@@ -6,5 +6,21 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		{
+			"s1n7ax/nvim-window-picker",
+			version = "2.*",
+			config = function()
+				require("window-picker").setup({
+					filter_rules = {
+						include_current_win = false,
+						autoselect_one = true,
+						bo = {
+							filetype = { "neo-tree", "neo-tree-popup", "notify" },
+							buftype = { "terminal", "quickfix" },
+						},
+					},
+				})
+			end,
+		},
 	},
 }
