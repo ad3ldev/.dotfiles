b581a19e4064f08a115cd9c582f973f4ea995deb
AbdelRahman Adel AbdelFatttah <ad3ldev@gmail.com>
2025-06-25 23:15:13 +0300

    fix: place of mason

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -185,7 +185,7 @@ nvim_lspconfig.harper_ls.setup({})
 -- JAVA CONFIG
 nvim_lspconfig.jdtls.setup({})
 local config = {
-	cmd = { vim.fn.expand("~/.local/nvim/mason/bin/jdtls") },
+	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
